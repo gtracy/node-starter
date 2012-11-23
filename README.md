c5c1a2099bde0cae08ab65c30d7e64ab24f5c882
gtracy <gtracy@gmail.com>
2012-11-23 12:19:17 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node Starter
+
+This project is designed to be a starter shell for new Node.js projects. I defaults to the [jade](http://jade-lang.com/) template engine.
+
+## Start server
+	node app.js
+
+## Browser view
+	http://localhost:8088
+	
---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+var logme = require('logme');
+var express = require('express');
+var fs = require('fs');
+
+
+// create the express web application server
+var app = express.createServer();
+
+app.configure(function() {
+
+    app.use(express.errorHandler({ dumpExceptions: true }));
+    app.use(express.logger(':date :method :url HTTP/:http-version :status :res[content-length] - :response-time ms'));
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    // set the favicon
+    app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
+
+    // parse JSON requests
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express['static'](__dirname + '/public'));
+    app.use(express.cookieParser());
+    app.use(express.session( {'secret' : 'hackme'} ));
+    app.use(app.router);
+
+    require('./routes/pages')(app);
+
+});
+
+// start the web application server
+var port = process.env.PORT || 8088;
+app.listen(port);
+logme.info('Asthmapolis server started on port ' + port);
